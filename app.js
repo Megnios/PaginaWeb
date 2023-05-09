d2878a4824fd87f533d5ab3b4b6eb6a416337a77
Megnios <nbalciunas@fi.uba.ar>
2023-05-09 15:54:13 -0300

    que se yo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const xhr = new XMLHttpRequest();
 //const url = "https://api.github.com/repos/Megnios/Megnios.github.io/contents/HTML";
-const url = "https://api.github.com/Megnios";
+const url = "https://api.github.com/users/Megnios";
 const token = "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";
 
 xhr.open("GET", url, true);
