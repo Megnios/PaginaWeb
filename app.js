f3b1e11402efef57e92855a13f61464922776c0d
Megnios <nbalciunas@fi.uba.ar>
2023-05-09 16:57:16 -0300

    intento con esto

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-import { Octokit } from "octokit";
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit();
 
-const octokit = new Octokit({ 
-  auth: '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',
-});
+const owner = "Megnios";
+const repo = "PaginaWeb";
+const path = "HTML"; // ruta de la carpeta que contiene los archivos HTML
 
-async function getUserInfo() {
-  try {
-    const { data } = await octokit.users.getByUsername({
-      username: "Megnios"
-    });
-    console.log(data);
-  } catch (error) {
-    console.error(error);
+async function getHtmlFiles() {
+  const iterator = octokit.paginate.iterator(octokit.rest.repos.getContent, {
+    owner,
+    repo,
+    path,
+  });
+
+  const htmlFiles = [];
+
+  for await (const response of iterator) {
+    const files = response.data;
+
+    // Filtramos los archivos que son HTML
+    const htmlFilesInPage = files.filter((file) => file.name.endsWith(".html"));
+
+    // Agregamos los archivos HTML a la lista
+    htmlFiles.push(...htmlFilesInPage);
   }
+
+  return htmlFiles;
 }
 
-getUserInfo();
+(async () => {
+  const htmlFiles = await getHtmlFiles();
+  console.log(htmlFiles);
+})();
