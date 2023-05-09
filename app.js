bfe8dc8e80741ec30437b72c967f8bcc0541e552
Megnios <nbalciunas@fi.uba.ar>
2023-05-09 15:43:39 -0300

    que se yo

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-// Hacer una solicitud a la API de GitHub para obtener la lista de archivos
-$.getJSON('https://api.github.com/repos/Megnios/Megnios.github.io/contents/HTML', function(data) {
+const xhr = new XMLHttpRequest();
+const url = "https://api.github.com/repos/Megnios/Megnios.github.io/contents/HTML";
+const token = "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";
 
-  // Recorrer la lista de archivos
-  $.each(data, function(index, file) {
+xhr.open("GET", url, true);
+xhr.setRequestHeader("Authorization", "token " + token);
 
-    // Verificar si el archivo es un archivo HTML
-    if (file.name.endsWith('.html')) {
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    //const files = JSON.parse(xhr.responseText);
+    // Aquí puedes procesar los archivos HTML y agregarlos dinámicamente a tu sitio web
+    console.log("hola");
+  } else {
+    console.log("Error al obtener archivos HTML: " + xhr.statusText);
+  }
+};
 
-      // Crear un nuevo elemento <div> para el archivo y agregarlo a la página
-      var newDiv = $('<div>').appendTo('body');
+xhr.onerror = function() {
+  console.log("Error de red al obtener archivos HTML");
+};
 
-      // Cargar el contenido del archivo HTML en el nuevo elemento <div>
-      newDiv.load(file.download_url);
-    }
-  });
-});
+xhr.send();
