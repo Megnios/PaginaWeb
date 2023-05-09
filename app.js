d1ce5aaf4dd70dd2d55e3d9f7f00a45d817b01b4
Megnios <nbalciunas@fi.uba.ar>
2023-05-09 16:21:14 -0300

    que se yo

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-const xhr = new XMLHttpRequest();
-//const url = "https://api.github.com/repos/Megnios/Megnios.github.io/contents/HTML";
-const url = "https://api.github.com/users/Megnios";
-const token = "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";
+import { Octokit } from "octokit";
 
-xhr.open("GET", url, true);
-xhr.setRequestHeader("Authorization", "token " + token);
+const octokit = new Octokit({ 
+  auth: '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',
+});
 
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    //const files = JSON.parse(xhr.responseText);
-    // Aquí puedes procesar los archivos HTML y agregarlos dinámicamente a tu sitio web
-    console.log("hola");
-  } else {
-    console.log("mal");
-    //console.log("Error al obtener archivos HTML: " + xhr.statusText);
+async function getUserInfo() {
+  try {
+    const { data } = await octokit.users.getByUsername({
+      username: "Megnios"
+    });
+    console.log(data);
+  } catch (error) {
+    console.error(error);
   }
-};
+}
 
-xhr.onerror = function() {
-  console.log("Error de red al obtener archivos HTML");
-};
-
-xhr.send();
+getUserInfo();
