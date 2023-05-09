5986547f1213417318b0f97d1765d0c4dd364edb
Megnios <nbalciunas@fi.uba.ar>
2023-05-09 17:07:21 -0300

    intento con esto

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 import { Octokit } from "octokit";
 
-const octokit = new Octokit();
-
-async function fetchHTMLFiles() {
-  const options = octokit.repos.listContents.endpoint.merge({
-    owner: 'Megnios',
-    repo: 'PaginaWeb',
-    path: 'HTML',
-  });
-
-  const response = await octokit.paginate.iterator(options);
-
-  for await (const { data } of response) {
-    const files = data.filter(item => item.type === 'file' && item.name.endsWith('.html'));
-
-    for (const file of files) {
-      const fileUrl = file.download_url;
-      // Aquí puedes hacer lo que necesites con la URL del archivo
-      console.log(fileUrl);
-    }
-  }
-}
-
-fetchHTMLFiles();
+const octokit = new Octokit({ 
+  auth: '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',
+});
